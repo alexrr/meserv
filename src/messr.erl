17049cb280a5d7905e76267be32da8a329abd3ad
alexrr <alexradchuk@yandex.ru>
2015-05-06 07:39:56 +1000

    next test

--- src/messr.erl
+++ src/messr.erl
@@ -19,19 +19,19 @@ init([]) ->
 ppr() ->
   receive
     {From, Counter, do_a_flip} ->
-      printMsg_recive("~p: recieved [do_a_flip] id[~p] from ~p\n", [self(), Counter, From]);
+      write_log_Msg("~p: recieved [do_a_flip] id[~p] from ~p\n", [self(), Counter, From]);
     {From, Counter, fish} ->
-      printMsg_recive("~p: recieved [fish] id[~p] from ~p\n", [self(), Counter, From]);
+      write_log_Msg("~p: recieved [fish] id[~p] from ~p\n", [self(), Counter, From]);
     {From, Counter, A} when is_atom(A) ->
-      printMsg_recive("~p: recieved atom[~p] id[~p] from ~p\n", [self(), A,Counter, From]);
+      write_log_Msg("~p: recieved atom[~p] id[~p] from ~p\n", [self(), A,Counter, From]);
     {From, Counter, A} when is_integer(A) ->
-      printMsg_recive("~p: recieved int[~p] id[~p] from ~p\n", [self(), A,Counter, From]);
+      write_log_Msg("~p: recieved int[~p] id[~p] from ~p\n", [self(), A,Counter, From]);
     _ ->
-      printMsg_recive("~p: recieved uknown message\n", [self()])
+      write_log_Msg("~p: recieved uknown message\n", [self()])
   end,
   ppr().
 
-printMsg_recive(Str, Args) ->
+write_log_Msg(Str, Args) ->
   Fmt = lists:flatten(io_lib:format(Str, Args)),
   io:fwrite(Fmt),
   gslogger:log_str(Fmt),
