484474b078f3006bec40e59001fb48ffa95be755
alexrr <alexradchuk@yandex.ru>
2015-06-06 11:10:48 +1000

    next test 33

--- src/messr.erl
+++ src/messr.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @author child
+%%% @author alexrr
 %%% @copyright (C) 2015, <COMPANY>
 %%% @doc
 %%%
@@ -13,23 +13,33 @@
 -export([init/1]).
 
 init([]) ->
-  Pid1 = spawn(fun() -> ppr() end),
+  Pid1 = spawn(fun() -> ppr(0) end),
   {ok, Pid1}.
 
-ppr() ->
-  receive
-    {From, Counter, do_a_flip} ->
-      write_log_Msg("~p: recieved [do_a_flip] id[~p] from ~p\n", [self(), Counter, From]);
-    {From, Counter, fish} ->
-      write_log_Msg("~p: recieved [fish] id[~p] from ~p\n", [self(), Counter, From]);
-    {From, Counter, A} when is_atom(A) ->
-      write_log_Msg("~p: recieved atom[~p] id[~p] from ~p\n", [self(), A,Counter, From]);
-    {From, Counter, A} when is_integer(A) ->
-      write_log_Msg("~p: recieved int[~p] id[~p] from ~p\n", [self(), A,Counter, From]);
-    _ ->
-      write_log_Msg("~p: recieved uknown message\n", [self()])
-  end,
-  ppr().
+ppr(CountWait) ->
+  if CountWait > 5 ->
+    write_log_Msg("~p: wait for ~p times, end working", [self(), CountWait]);
+    true ->
+      receive
+        {From, Counter, do_a_flip} ->
+          write_log_Msg("~p: recieved [do_a_flip] id[~p] from ~p\n", [self(), Counter, From]), ppr(0);
+        {From, Counter, fish} ->
+          write_log_Msg("~p: recieved [fish] id[~p] from ~p\n", [self(), Counter, From]), ppr(0);
+        {From, Counter, A} when is_atom(A) ->
+          write_log_Msg("~p: recieved atom[~p] id[~p] from ~p\n", [self(), A, Counter, From]), ppr(0);
+        {From, Counter, A} when is_integer(A) ->
+          write_log_Msg("~p: recieved int[~p] id[~p] from ~p\n", [self(), A, Counter, From]), ppr(0);
+        {From,  stop} ->
+          write_log_Msg("~p: recieved stop message\n", [self()]),
+          From ! {self(),stop_ack},
+          exit(0);
+        _ ->
+          write_log_Msg("~p: recieved uknown message\n", [self()]), ppr(0)
+      after 20 ->
+        write_log_Msg("~p: no message ~p\n", [self(), CountWait]),
+        ppr(CountWait + 1)
+      end
+  end.
 
 write_log_Msg(Str, Args) ->
   Fmt = lists:flatten(io_lib:format(Str, Args)),
