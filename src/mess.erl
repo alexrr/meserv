0213253979690fab9a023581587ca6cc4679c093
alexrr <alexradchuk@yandex.ru>
2015-06-06 11:10:12 +1000

    next test 33

--- src/mess.erl
+++ src/mess.erl
@@ -19,12 +19,12 @@ start() ->
   gslogger:log_str(ok),
   gslogger:log_str("Start sender"),
   {ok, Pid1} = messr:init([]),
-  Pid2 = spawn(fun() -> sender(Pid1, 0, 0) end),
+  Pid2 = spawn(fun() -> sender(Pid1, 0, 100, 0) end),
   io:format(" Process reciever ~p started\n Process sender ~p started\n", [Pid1, Pid2]).
 
-sender(Pid, TypeMsg, Counter) ->
+sender(Pid, TypeMsg, MaxCounter, Counter) ->
   write_log_Msg("~p: try send MSG: id=[~p] type=[~p] from ~p\n", [self(), Counter, TypeMsg, Pid]),
-  if Counter < 15 ->
+  if Counter < MaxCounter ->
     case TypeMsg of
       0 -> Pid ! {self(), Counter, do_a_flip}, NextMsg = 1;
       1 -> Pid ! {self(), Counter, fish}, NextMsg = 2;
@@ -32,10 +32,22 @@ sender(Pid, TypeMsg, Counter) ->
       3 -> Pid ! {self(), Counter, 555}, NextMsg = 0;
       _ -> NextMsg = 0
     end,
-    sleep(5),
-    sender(Pid, NextMsg, Counter + 1);
+    if Counter rem 12 == 0->  sleep(25);
+      true ->  sleep(5)
+    end,
+    sender(Pid, NextMsg, MaxCounter, Counter + 1);
     true ->
-      write_log_Msg("~p: Counter exeed", [self()])
+      stopping(Pid)
+  end.
+
+stopping(Pid) ->
+  write_log_Msg("~p: Counter exeed \n", [self()]),
+  write_log_Msg("~p: Sending stop msg to ~p \n", [self(), Pid]),
+  Pid ! {self(), stop},
+  receive
+    {RPid, stop_ack} -> write_log_Msg("~p: Recicer ~p stopped ok \n", [self(), RPid])
+  after 200 ->
+    write_log_Msg("~p: Recicer NOT stopped\n", [self()])
   end.
 
 write_log_Msg(Str, Args) ->
